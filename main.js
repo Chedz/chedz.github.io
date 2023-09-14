97d486f28fa883668b5583f45ff95034c7216545
Eoin Chedzey <eoinisnow@gmail.com>
2023-09-14 23:07:31 +0100

    added in 3 spheres with independent matrices for animation. Removed unused code and moved to seperate .js file

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 import * as THREE from 'https://cdn.skypack.dev/three';
-// import * as OrbitControls from "https://cdn.jsdelivr.net/npm/three-orbitcontrols@2.110.3/OrbitControls.min.js";
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
 
-//Generate an array of all the colors from the rainbow
-// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
 let scene, renderer, camera;
+globalThis.THREE= THREE;
+
+let sphere;
+let matricesColorInterval = [0,0,0];
 
 function initScene(){
     scene = new THREE.Scene();
+    globalThis.scene = scene;
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
     scene.fog = new THREE.Fog(0x000000, 500, 10000)
 
@@ -34,12 +37,213 @@ function addBox(){
     scene.add( cube );
 }
 
-function addSphere(){
-    const geometry = new THREE.SphereGeometry(1, 16, 16 )
+function addSphere(radius=1, widthSegments=16, heightSegments=16){
+    const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments )
     const material = new THREE.MeshLambertMaterial( { color: 0x00ff00, wireframe: true } );
-    const sphere = new THREE.Mesh( geometry, material );
+    sphere = new THREE.Mesh( geometry, material );
     scene.add( sphere );
+
 }
+
+globalThis.addSphere = addSphere;
+
+
+
+let columnCount = 0;
+let matrix = [];
+let matricesHolder =[];
+globalThis.matricesHolder = matricesHolder;
+
+function drawCircle(radius, rowCount, zVal = 0){
+    pointsList = [];
+    // drawPoint();
+    // drawPoint(1,1,1);
+
+    let removeTopIndex;
+    let removeBottomIndex;
+    let vCount;
+
+    if(rowCount%2){ 
+        // console.log("odd")
+        vCount = (rowCount+1)*2;
+        removeTopIndex = vCount/4;
+        removeBottomIndex = ((3*vCount)/4)-1;
+    }else{
+        // console.log("even")
+        vCount = (rowCount*2) + 1;
+        removeTopIndex = vCount/4;
+        removeBottomIndex = ((3*vCount)/4);
+    }
+
+    // vCount = (rowCount+1)*2;
+
+    let angle = 360.0 / vCount;
+
+    let triangleCount = vCount - 2;
+
+    let temp = []; //stores Vec3
+    let vertices = [];
+
+    // positions
+    for (let i = 0; i < vCount; i++)
+    {
+        // console.log("drawCircle: "+ radius);
+        let currentAngle = angle * i;
+        let x = radius * Math.cos((currentAngle * Math.PI) / 180.0); ;
+        let y = radius * Math.sin((currentAngle * Math.PI) / 180.0);
+        let z = zVal;
+
+        temp.push(new THREE.Vector3(x, y, z));
+    }
+
+    for (let i = 0; i < triangleCount; i++)
+    {
+        vertices.push(temp[0]);
+        vertices.push(temp[i + 1]);
+        vertices.push(temp[i + 2]);
+    }
+
+    // console.log(vertices);
+
+    let cleanedVertices = vertices.filter((value, index, self) =>
+        index === self.findIndex((t) => (
+            t.x === value.x && t.y === value.y
+        ))
+    );
+
+
+    cleanedVertices.splice(removeTopIndex,1);
+    cleanedVertices.splice(removeBottomIndex,1);
+
+    let col1 = cleanedVertices.splice(removeTopIndex, removeBottomIndex - removeTopIndex); //this is one side of the circle (not used, since we want one col to be half a circle)
+    // console.log(col1)
+    // console.log(cleanedVertices);
+
+    // console.log(cleanedVertices);
+
+    for (let index = 0; index < col1.length; index++) {
+        drawPoint(col1[index].x, col1[index].y, col1[index].z)
+    }
+
+    matrix[columnCount] = pointsList;
+    columnCount++;
+
+    // console.log(pointsList.length)
+
+}
+function rotateAlongAxis(inputArray, angle){
+    for (let index = 0; index < inputArray.length; index++) {
+        const element = inputArray[index];
+        element.rotateOnWorldAxis(new THREE.Vector3(0,1,0), angle);
+    }
+}
+
+
+globalThis.rotateAlongAxis = rotateAlongAxis;
+
+globalThis.drawCircle = drawCircle;
+
+let pointsList =[];
+globalThis.pointsList = pointsList;
+
+function drawPoint(x=0, y=0, z=0){
+    // console.log('draw');
+    let bufferGeom = new THREE.BufferGeometry();
+    let bufferMaterial = new THREE.PointsMaterial({ size: 0.1, color: 0x0000ff });
+
+    let point = new THREE.Points(bufferGeom, bufferMaterial);
+
+    scene.add(point);
+    pointsList.push(point);
+    
+    bufferGeom.setAttribute("position", new THREE.Float32BufferAttribute(new THREE.Vector3(x,y,z), 3));
+}
+
+function createMatrix(numOfCol=8, numOfRow=12, circleRadius=1,){
+    addSphere(circleRadius, numOfCol, numOfRow) //(radius=1, widthSegments=16, heightSegments=16)
+    columnCount=0;
+    matrix = [];
+    for (let index = 0; index < numOfCol; index++) {
+        // console.log("create: " + index)
+        drawCircle(circleRadius,numOfRow,0); //radius, vCount, zVal = 0
+        // if(index > 0) rotateAlongAxis(matrix[index], ((2*Math.PI) / (numOfCol)) * index)
+        if(index > 0) rotateAlongAxis(matrix[index], index * ((2*Math.PI)/numOfCol ));
+
+    }
+    matricesHolder.push(matrix);
+}
+globalThis.createMatrix = createMatrix;
+
+function getPoint(row, column, colorVal){
+    let point = matrix[column][row];
+    return point;
+}
+globalThis.getPoint = getPoint;
+
+function animation(matrixIndex){
+    let matrixAnimated = matricesHolder[matrixIndex];
+    if(matrixAnimated.length==0) return;
+
+    let redVal = 0;
+    let blueVal = 0;
+    let greenVal = 0;
+
+    let colorInterval = matricesColorInterval[matrixIndex];
+
+    // console.log("matrix index: " + matrixIndex)
+
+
+    switch (colorInterval) {
+        case 0: //red
+            redVal = 1;
+            blueVal = 0;
+            greenVal = 0;
+
+            matricesColorInterval[matrixIndex]++;
+            break;
+
+        case 1: //green
+            redVal = 0;
+            blueVal = 0;
+            greenVal = 1;
+
+            matricesColorInterval[matrixIndex]++;
+            break;
+
+        case 2: //blue
+            redVal = 0;
+            blueVal = 1;
+            greenVal = 0;
+
+            matricesColorInterval[matrixIndex] = 0;
+            break;
+    
+        default:
+
+            break;
+    }
+
+    for (let rowIndex = 0; rowIndex < matrixAnimated[0].length; rowIndex++) {
+        // const element = array[index];
+        for (let colIndex = 0; colIndex < matrixAnimated.length; colIndex++) {
+            // const element = array[index];
+            let point = matrixAnimated[colIndex][rowIndex];
+
+            setTimeout(function timer() {
+                // console.log("hello world");
+                point.material.color.r = redVal;
+                point.material.color.g = greenVal;
+                point.material.color.b = blueVal;
+            }, rowIndex * 100);
+           
+            
+        }
+        
+    }
+}
+
+globalThis.animation = animation;
+
 
 function initLights(){
     // Lights
@@ -67,20 +271,37 @@ function initLights(){
     scene.add(directionalLight)
 }
 
-function updateCamera() {
-    camera.updateProjectionMatrix();
-}
 
 function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+    // point.rotation.y += 0.006;
     // updateCamera();
 }
 
 
 initScene();
 initLights();
+// drawCircle();
+// createPoints();
 // addBox();
-addSphere();
-animate();
+// let col1 = createColumn();
+// globalThis.col1 = col1;
+// addSphere();
+// createMatrix(8);
+// setInterval(function() {
+//     animation();
+// }, 2000);
 
+createMatrix(8,12,1);
+createMatrix(8,12,1.5);
+createMatrix(8,12,2);
+
+setInterval(function() {
+    animation(0);
+    animation(1);
+    animation(2);
+}, 2000);
+
+
+animate();
